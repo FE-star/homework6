ba774df5fb30e103789a3a0ecd352ae39c233343
yangxiaolong <yangxiaolong@fanghuwang.com>
2017-09-28 21:20:05 +0800

    第三版

--- plugin.js
+++ plugin.js
@@ -1,42 +1,17 @@
 const ConcatSource = require('webpack-sources').ConcatSource
 
 class DefPlugin {
-    constructor(name) {}
+
+    constructor(name) {
+        this.name = name;
+    }
 
     apply(compiler) {
-        compiler.plugin("emit", function(compilation, callback) {
-            // 探索每个块（构建后的输出）:
-            compilation.chunks.forEach(function(chunk) {
-                // 探索块中的每个模块（构建时的输入）：
-                /*chunk.forEachModule(function(module) {
-                    // 探索模块中包含的的每个源文件路径：
-                    module.fileDependencies.forEach(function(filepath) {
-                        // 现在我们已经知道了很多的源文件结构了……
-                        console.log("filepath==" + filepath);
-                    });
-                });*/
-
-                // 探索块生成的每个资源文件名
-                chunk.files.forEach(function(filename) {
-                    // 得到块生成的每个文件资源的源码
-                    var source = compilation.assets[filename].source();
-
-                    source = `global.define(['module', 'exports'], function(module, exports){
-                                 ${source}
-                            });
-                            `
-                    compilation.assets[filename] = {
-                        source: function() {
-                            return source;
-                        },
-                        size: function() {
-                            return source;
-                        }
-                    };
-                });
+        
+        compiler.plugin('compilation', function(compilation) {
+            compilation.templatesPlugin("render-with-entry", (source, chunk, hash) => {
+                 return new ConcatSource("define(['module', 'exports'],function(module, exports) { return ", source, "});");
             });
-
-            callback();
         });
     }
 }
