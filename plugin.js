388fd09ff15699fd5965af93763d2c2c22565f32
wind4gis <mid_autumn@126.com>
2017-10-07 00:14:38 +0800

    测试通过, 原地爆炸
    不是用的ConcatSource修改源码, 而是直接用OriginalSource修改从源文件读出来的source源码

--- plugin.js
+++ plugin.js
@@ -1,10 +1,28 @@
-const ConcatSource = require('webpack-sources').ConcatSource
+const OriginalSource = require('webpack-sources').OriginalSource,
+      path = require('path')
 
 class DefPlugin {
   constructor(name) {
   }
 
   apply(compiler) {
+    compiler.plugin('compilation', function (compilation) {
+      compilation.plugin('succeed-module',function(module){
+        let _name = path.join(__dirname, 'src', 'hello.js'), _value, _source
+        if(module.userRequest == _name){
+          _value = `module.exports = global.define([
+                        'require',
+                        'module',
+                        'exports'
+                      ], function(require, module, exports) {
+                        module['exports'] = 'hello world'
+                      })
+                    `
+          _source = new OriginalSource(_value, _name)
+          module._source = _source
+        }
+      })
+    })
   }
 }
 
