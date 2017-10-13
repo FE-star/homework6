9c8fe90d18a9eb9650f7196a23ae4bc9b480a9c9
dingyanping <dingyanping@haodai.com>
2017-10-13 23:01:18 +0800

    '测试通过'

--- Readme.md
+++ Readme.md
@@ -1,3 +1,8 @@
 # Readme
 
 > 写一个插件使得其能够生成 AMD 模块，可能需要看 webpack 源代码找到适合的生命周期
+
+
+## 参考
+- [plugin API](https://doc.webpack-china.org/api/plugins)
+- [如何写一个webpack插件](http://hao.jser.com/archive/16469/)
--- plugin.js
+++ plugin.js
@@ -1,11 +1,20 @@
 const ConcatSource = require('webpack-sources').ConcatSource
 
 class DefPlugin {
-  constructor(name) {
-  }
+    constructor(name) {}
 
-  apply(compiler) {
-  }
+    apply(compiler) {
+        compiler.plugin('emit', function(compilation, callback) {
+            let assets = compilation.assets
+            for (let key in compilation.assets) {
+                console.log(key);
+                let str = assets[key].source();
+                str = `global.define(['require', 'module', 'exports'], ( require, module, exports) => {${str}})`
+                assets[key] = new ConcatSource(str);
+            }
+            callback();
+        })
+    }
 }
 
 module.exports = DefPlugin
