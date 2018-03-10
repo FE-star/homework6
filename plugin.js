b09a899b8d2ec5e8bc1762035ea6990fc7b1d236
suozhao <suozhao@chehang168.com>
2018-03-11 01:32:11 +0800

    homework6

--- plugin.js
+++ plugin.js
@@ -1,11 +1,18 @@
 const ConcatSource = require('webpack-sources').ConcatSource
+let fs = require('fs')
 
 class DefPlugin {
-  constructor(name) {
-  }
+    constructor(name) {}
 
-  apply(compiler) {
-  }
+    apply(compiler) {
+        compiler.plugin('compilation', (compilation)=>{
+			compilation.mainTemplate.plugin('render-with-entry', function(source, chunk, hash){
+				return new ConcatSource(`global.define(['require','module','exports'],function(require, module, exports) { 
+					${source.source()} 
+				})`);
+			}) 
+	    })
+    }
 }
 
 module.exports = DefPlugin
