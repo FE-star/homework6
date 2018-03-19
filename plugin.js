8a5e94e05dcaef2829b494fbb510b679e4f7dd83
Ivan <wcy5211678@163.com>
2018-03-19 20:50:12 +0800

    plugin

--- plugin.js
+++ plugin.js
@@ -14,24 +14,24 @@ class DefPlugin {
        console.log("The compiler is starting a new compilation...");
 
 
+      //参考：https://webpack.js.org/api/compilation/
        compilation.plugin("optimize", function() {
          console.log("The compilation is starting to optimize files...");
        });
-     });
 
-     compiler.plugin("emit", function(compilation, callback) {
-       console.log("The compilation is going to emit files...");
-      let assets = compilation.assets;
-      for (const asset in assets) {
-        let source = assets[asset].source();
-        // console.log(source);
-        assets[asset] = new ConcatSource(
-          `define(["require", "module", "exports"], function(require,module,exports) { 
-            return ${source}
+      compilation.plugin("succeed-module", function(module) {
+        console.log("Successfully built: ", module);
+      });
+
+      // ....
+      
+      compilation.plugin("chunk-asset", function(chunk, filename) {
+        console.log("An asset from a chunk was added to the compilation ", chunk, filename);
+        compilation.assets[filename] = new ConcatSource(`define(["require", "module", "exports"], function(require,module,exports) { 
+            return ${compilation.assets[filename].source()}
           });`);
-      }
-       callback();
-     });
+      });
+    });
   }
 }
 
