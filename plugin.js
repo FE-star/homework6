7c04038e663b88b48f7c23288f637f06652a9375
Ivan <wcy5211678@163.com>
2018-03-19 19:41:36 +0800

    plugin

--- plugin.js
+++ plugin.js
@@ -5,6 +5,33 @@ class DefPlugin {
   }
 
   apply(compiler) {
+     compiler.plugin("compile", function(params) {
+      
+       console.log("The compiler is starting to compile...");
+     });
+
+     compiler.plugin("compilation", function(compilation) {
+       console.log("The compiler is starting a new compilation...");
+
+
+       compilation.plugin("optimize", function() {
+         console.log("The compilation is starting to optimize files...");
+       });
+     });
+
+     compiler.plugin("emit", function(compilation, callback) {
+       console.log("The compilation is going to emit files...");
+      let assets = compilation.assets;
+      for (const asset in assets) {
+        let source = assets[asset].source();
+        // console.log(source);
+        assets[asset] = new ConcatSource(
+          `define(["require", "module", "exports"], function(require,module,exports) { 
+            return ${source}
+          });`);
+      }
+       callback();
+     });
   }
 }
 
