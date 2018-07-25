5d0a780e54d7b3b803705b2bff95f5c440397d5e
zhuanyongxigua <zhuanyongxigua@gmail.com>
2018-07-25 16:39:41 +0800

    first finish

--- plugin.js
+++ plugin.js
@@ -5,6 +5,15 @@ class DefPlugin {
   }
 
   apply(compiler) {
+    compiler.plugin('emit', function(compilation, callback) {
+      var source = new ConcatSource;
+      source.add(compilation.assets['./bundle.js']._source)
+      source.add("global.define(['module'], function() {")
+      source.add("arguments[0].exports = module.exports")
+      source.add("})")
+      compilation.assets['./bundle.js']._source = source;
+      callback();
+    })
   }
 }
 
