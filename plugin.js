c03a35b2b6d59a3d4ef6667cf77f7aeb95955102
Jerry379 <876351098@qq.com>
2018-12-17 17:06:24 +0800

    text pass

--- plugin.js
+++ plugin.js
@@ -5,6 +5,22 @@ class DefPlugin {
   }
 
   apply(compiler) {
+    // emit（'编译器'对'生成最终资源'这个事件的监听）
+    compiler.plugin("emit", function(compilation, callback) {
+      // compilation.chunks是块的集合（构建后将要输出的文件，即编译之后得到的结果）
+      compilation.chunks.forEach(function(chunk) {
+        // 最终生成的文件的集合
+        chunk.files.forEach(function(filename) {
+          // source()可以得到每个文件的源码
+          var source = compilation.assets[filename].source();
+          compilation.assets[filename] =
+            new ConcatSource(
+              "define(['require', 'module', 'exports'],function(require, module, exports) { return ", source, "});"
+            );
+        });
+      })
+      callback();
+    })
   }
 }
 
