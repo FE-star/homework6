cda742605334cfe682c36ba37a8fb205698c1b72
ZhangLynn <zl8312070@sina.com>
2018-12-19 17:47:57 +0800

    feat: 跑通测试

--- plugin.js
+++ plugin.js
@@ -1,11 +1,25 @@
-const ConcatSource = require('webpack-sources').ConcatSource
-
+/**
+ * created by LynnZhang on 2018/12/15
+ */
 class DefPlugin {
-  constructor(name) {
-  }
-
-  apply(compiler) {
-  }
+    apply(compiler) {
+        compiler.plugin('emit', (compilation, callback) => {
+            let content = compilation.assets['./bundle.js'].source();
+            content = `global.define(['require','module','exports'], function(require, module, exports) {
+                     ${content}
+                 })`;
+            // 重写指定输出模块内容
+            compilation.assets['./bundle.js'] = {
+                source() {
+                    return content
+                },
+                size() {
+                    return content.length
+                }
+            }
+            callback()
+        })
+    }
 }
 
 module.exports = DefPlugin
