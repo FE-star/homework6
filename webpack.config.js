9bd3907648e40fc024ee2286989440eede6a6495
SimpleCodeCX <248200851@qq.com>
2018-12-23 22:05:22 +0800

    homework6 test passed

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@ const path = require('path')
 const Plugin = require('./plugin')
 
 module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: './index.js',
-    output: {
-        libraryTarget: 'commonjs2',
-        path: path.join(__dirname, 'dist'),
-        filename: './bundle.js'
-    },
-    plugins: [
-        new Plugin
-    ]
+  context: path.join(__dirname, 'src'),
+  entry: './index.js',
+  mode: 'production',
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, 'dist'),
+    filename: './bundle.js'
+  },
+  plugins: [
+    new Plugin
+  ]
 }
